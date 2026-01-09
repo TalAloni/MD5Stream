081b0adea72369b91439cf80a02aa5e60d8b71bf
Tal Aloni <tal.aloni.il@gmail.com>
2026-01-09 12:35:39 +0200

    Updated solution to support Visual Studio 2022

--- MD5Stream/ILMerge/ILMerge.bat
+++ MD5Stream/ILMerge/ILMerge.bat
@@ -1,6 +1,11 @@
-set binaryPath=%CD%\..\bin\Release
-set outputPath=%CD%\..\bin\ILMerge
-IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
 IF ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles%\Microsoft\ILMerge"
 IF NOT ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles(x86)%\Microsoft\ILMerge"
-%ilmergePath%\ilmerge /ndebug /target:winexe /out:"%outputPath%\MD5Stream.exe" "%binaryPath%\MD5Stream.exe" "%binaryPath%\System.Core.dll" "%binaryPath%\Trinet.Core.IO.Ntfs.dll"
+
+set TargetFramework=%1
+if %TargetFramework%==net20 set TargetPlaform=2.0
+if %TargetFramework%==net40 set TargetPlaform=4.0
+set binaryPath=%CD%\..\bin\Release\%TargetFramework%
+set outputPath=%CD%\..\bin\ILMerge\%TargetFramework%
+IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
+if %TargetFramework%==net20 %ilmergePath%\ilmerge /targetplatform=%TargetPlaform% /ndebug /target:winexe /out:"%outputPath%\MD5Stream.exe" "%binaryPath%\MD5Stream.exe" "%binaryPath%\System.Core.dll" "%binaryPath%\Trinet.Core.IO.Ntfs.dll"
+if %TargetFramework%==net40 %ilmergePath%\ilmerge /targetplatform=%TargetPlaform% /ndebug /target:winexe /out:"%outputPath%\MD5Stream.exe" "%binaryPath%\MD5Stream.exe" "%binaryPath%\Trinet.Core.IO.Ntfs.dll"
